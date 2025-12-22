466ce8fce22ea68121d50c766a063c81a2f9c3b3
mauri <ruizzmaury@gmail.com>
2025-12-22 14:50:53 +0100

    base setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return  {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "rust", "javascript", "zig", "go" },
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+}
