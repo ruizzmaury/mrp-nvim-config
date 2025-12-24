cae659441ad1e369164977f3694381a9fb7609de
Mauri Ruiz <mauri.ruiz@webbeds.com>
2025-12-24 10:32:37 +0100

    adding line numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.g.maplocalleader = "\\"
 vim.g.mapleader = " "
 
