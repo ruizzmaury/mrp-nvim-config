f0e47fd8c2809bdbc97583d82e76bea32035fcde
Mauri Ruiz <mauri.ruiz@webbeds.com>
2026-01-15 16:39:16 +0100

    smear + copilot nvim plugins

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,21 +3,46 @@ return {
   cmd = "Copilot",
   build = ":Copilot auth",
   event = "BufReadPost",
+
   opts = {
     suggestion = {
-      enabled = not vim.g.ai_cmp,
+      enabled = true,
       auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
+      hide_during_completion = false,
+
       keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
+        accept = false, -- we bind it manually
         next = "<M-]>",
         prev = "<M-[>",
+        dismiss = "<C-x>",
       },
     },
-    panel = { enabled = false },
+
+    panel = {
+      enabled = false,
+    },
+
     filetypes = {
       markdown = true,
       help = true,
     },
   },
+
+  config = function(_, opts)
+    require("copilot").setup(opts)
+
+    local suggestion = require("copilot.suggestion")
+
+    -- Accept Copilot inline suggestion
+    vim.keymap.set("i", "<C-l>", function()
+      if suggestion.is_visible() then
+        suggestion.accept()
+      end
+    end, { silent = true, desc = "Accept Copilot suggestion" })
+
+    -- Manually trigger Copilot suggestion
+    vim.keymap.set("i", "<C-/>", function()
+      suggestion.next()
+    end, { silent = true, desc = "Trigger Copilot suggestion" })
+  end,
 }
