14c13d65028e37c270d35bc0ee5dfd6ce13a1aad
mauri <ruizzmaury@gmail.com>
2026-01-16 08:40:02 +0100

    smear goz

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -1,4 +1,19 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opt = {},
+  event = "VeryLazy",
+  cond = vim.g.neovide == nil,
+  opts = {
+    hide_target_hack = true,
+    cursor_color = "none",
+  },
+  specs = {
+    -- disable mini.animate cursor
+    {
+      "nvim-mini/mini.animate",
+      optional = true,
+      opts = {
+        cursor = { enable = false },
+      },
+    },
+  },
 }
